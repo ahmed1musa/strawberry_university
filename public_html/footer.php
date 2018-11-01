f85781176df32212ca8697a4fa68fd8d845a546d
Ahmed <43175307+ahmed1musa@users.noreply.github.com>
2018-10-31 22:31:15 -0500

    php modifications

---
+++ public_html/footer.php
@@ -0,0 +1,7 @@
+<footer>
+<p> 
+  <img alt='Strawberry' src='img/strawberry.png' width=71px height=47px> 
+    &copy; Copyright 2018. Ahmed Musa, Carter Irlmeier, Jadon Jahnke
+  <img alt='Strawberry' src='img/strawberry.png' width=71px height=47px>
+</p>
+</footer>
